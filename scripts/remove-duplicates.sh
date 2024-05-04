ee479bd827935b95d5d1844e4b4513140812c871
Ren Tatsumoto <tatsu@autistici.org>
2024-05-04 19:02:29 +0300

    add remove duplicates step
    Former-commit-id: 117c9a8eea7a4c95f08e9b01862d8412960655b0 [formerly ec258ed5cf32a2df9636cf64d9cd456f1d958185]
    Former-commit-id: 3e8b6aaafe8ffbd0ab5ad7e4b8493f9e7daeb55c

---
+++ scripts/remove-duplicates.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+
+readonly target_dir=subtitles
+readonly ignore_file=.kitsuignore
+
+main() {
+	if ! cd -- "$target_dir"; then
+		echo "can't cd to $target_dir"
+		exit 1
+	fi
+
+	if ! [[ -f .kitsuignore ]]; then
+		echo "directory doesn't look right."
+		exit 1
+	fi
+
+	if ! command -v fdupes; then
+		echo "fdupes is not installed."
+		exit 1
+	fi
+
+	fdupes -rdNI --order=time --reverse .
+}
+
+main "$@"
